06ec42c59be0787d7abf177abb87702c2159c2eb
Javier Arrieta <javier@techdelivery.es>
2026-02-21 12:50:59 +0100

    remove autocomplete-server alias

--- modules/home-manager/shell.nix
+++ modules/home-manager/shell.nix
@@ -83,10 +83,6 @@
       "kx" = "kubectx";
       "venv" = "python3 -m venv";
       "rebase-pr" = "git fetch && git merge origin/${userOptions.gitDefaultBranch} && git push";
-      "autocomplete-server" =
-        "hf download sweepai/sweep-next-edit-1.5B sweep-next-edit-1.5b.q8_0.v2.gguf --local-dir $HOME/llm/models && \
-         llama-server -m $HOME/llm/models/sweep-next-edit-1.5b.q8_0.v2.gguf --offline --jinja -ngl 99 --threads -1 --ctx-size 8192 --port 8081";
-    };
   };
 
   programs.starship = {
