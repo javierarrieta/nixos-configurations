8005e6a8b3222347a4f9e90d804296f64c7b80c3
Javier Arrieta <javier@techdelivery.es>
2026-02-21 17:16:02 +0100

    fix

--- hosts/llm01/configuration.nix
+++ hosts/llm01/configuration.nix
@@ -87,7 +87,7 @@
 
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelParams = [ "amdgpu.sched_policy=2 amd_iommu=off" ];
+  boot.kernelParams = [ "amdgpu.sched_policy=2" "amd_iommu=off" ];
   boot.extraModprobeConfig = ''
     # Allocate more memory to the GPU VRAM for llama.cpp
     options amdgpu gttsize=120000
