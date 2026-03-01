5df25256d2a031861f0452efda2b79c3a4b6c1ba
Javier Arrieta <javier@techdelivery.es>
2026-03-01 18:10:13 +0100

    extract models to its own config

--- hosts/llm01/configuration.nix
+++ hosts/llm01/configuration.nix
@@ -11,37 +11,7 @@
 let
   # This points to the specific Vulkan package from the flake
   llamaPackage = llama-cpp.packages.${pkgs.stdenv.hostPlatform.system}.vulkan;
-
-  models = {
-    "Qwen2.5-coder-1.5B" = {
-      modelId = "Qwen/Qwen2.5-Coder-1.5B-Instruct-GGUF";
-      filename = "qwen2.5-coder-1.5b-instruct-q4_k_m.gguf";
-      extraProperties = {
-        "ctx-size" = "8192";
-      };
-    };
-    "Qwen3.5-35B" = {
-      modelId = "unsloth/Qwen3.5-35B-A3B-GGUF";
-      filename = "Qwen3.5-35B-A3B-UD-Q4_K_XL.gguf";
-      extraProperties = {
-        "ctx-size" = "65386";
-      };
-    };
-    "MiroThinker-v1.5-30B" = {
-      modelId = "mradermacher/MiroThinker-v1.5-30B-GGUF";
-      filename = "MiroThinker-v1.5-30B.Q4_K_M.gguf";
-      extraProperties = {
-        "ctx-size" = "65386";
-      };
-    };
-    "Qwen3-coder-Next" = {
-      modelId = "unsloth/Qwen3-Coder-Next-GGUF";
-      filename = "Qwen3-Coder-Next-Q4_K_M.gguf";
-      extraProperties = {
-        "ctx-size" = "65386";
-      };
-    };
-  };
+  models = import ./llm-models.nix;
 in
 {
   imports = [
---
+++ hosts/llm01/llm-models.nix
@@ -0,0 +1,30 @@
+{
+  "Qwen2.5-coder-1.5B" = {
+    modelId = "Qwen/Qwen2.5-Coder-1.5B-Instruct-GGUF";
+    filename = "qwen2.5-coder-1.5b-instruct-q4_k_m.gguf";
+    extraProperties = {
+      "ctx-size" = "8192";
+    };
+  };
+  "Qwen3.5-35B" = {
+    modelId = "unsloth/Qwen3.5-35B-A3B-GGUF";
+    filename = "Qwen3.5-35B-A3B-UD-Q4_K_XL.gguf";
+    extraProperties = {
+      "ctx-size" = "65386";
+    };
+  };
+  "MiroThinker-v1.5-30B" = {
+    modelId = "mradermacher/MiroThinker-v1.5-30B-GGUF";
+    filename = "MiroThinker-v1.5-30B.Q4_K_M.gguf";
+    extraProperties = {
+      "ctx-size" = "65386";
+    };
+  };
+  "Qwen3-coder-Next" = {
+    modelId = "unsloth/Qwen3-Coder-Next-GGUF";
+    filename = "Qwen3-Coder-Next-Q4_K_M.gguf";
+    extraProperties = {
+      "ctx-size" = "65386";
+    };
+  };
+}
