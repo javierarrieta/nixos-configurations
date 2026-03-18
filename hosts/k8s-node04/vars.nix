9e7ac57c265fdcabc1569706eab158152e1a0a92
Javier Arrieta <javier@techdelivery.es>
2026-03-18 09:53:06 +0000

    k8s-node04

---
+++ hosts/k8s-node04/vars.nix
@@ -0,0 +1,27 @@
+{ config, pkgs }:
+{
+  hostname = "k8s-node04";
+  ipAddress = "$IP_ADDRESS";
+  defaultGateway = "$DEFAULT_GATEWAY";
+  nameservers = [
+    "$DNS1"
+    "$DNS2"
+  ];
+
+  k3sOptions = {
+    enable = true;
+    role = "agent";
+    extraFlags = toString [
+      #"--node-label node-role.kubernetes.io/worker=true"
+      "--node-label storage=ssd"
+      "--node-label arch=amd64"
+      "--node-label cpu=n100"
+    ];
+    tokenFile = config.sops.secrets."k8s-node04/k3s_token".path;
+    serverAddr = "https://$SERVER_ADDR:6443";
+  };
+
+  lokiPromtailClient = {
+    url = "$LOKI_URL";
+  };
+}
