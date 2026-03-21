b34227cec9f4159d8890b3523c82dc14bf1079f7
Javier Arrieta <javier@techdelivery.es>
2026-03-21 08:21:49 +0000

    [llm01] Qwen3.5-122B-A10B

--- hosts/llm01/llm-models.nix
+++ hosts/llm01/llm-models.nix
@@ -21,6 +21,13 @@
       "ctx-size" = "65536";
     };
   };
+  "Qwen3.5-122B-A10B" = {
+    modelId = "unsloth/Qwen3.5-122B-A10B-GGUF";
+    filename = "Qwen3.5-122B-A10B-Q4_K_M-00001-of-00003.gguf";
+    extraProperties = {
+      "ctx-size" = "120000";
+    };
+  };
   "Mistral-Small-4-119B-2603" = {
     modelId = "unsloth/Mistral-Small-4-119B-2603-GGUF";
     filename = "UD-Q4_K_XL/Mistral-Small-4-119B-2603-UD-Q4_K_XL-00001-of-00003.gguf";
