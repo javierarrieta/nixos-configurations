6d21396272cd6a240a5c3508c7c5d67a48f2e1ec
Javier Arrieta <javier@techdelivery.es>
2026-03-21 09:11:04 +0000

    [llm01] disable mistral4, enable qwen3.5-122B

--- hosts/llm01/llm-models.nix
+++ hosts/llm01/llm-models.nix
@@ -23,16 +23,16 @@
   };
   "Qwen3.5-122B-A10B" = {
     modelId = "unsloth/Qwen3.5-122B-A10B-GGUF";
-    filename = "Qwen3.5-122B-A10B-Q4_K_M-00001-of-00003.gguf";
+    filename = "Q4_K_M/Qwen3.5-122B-A10B-Q4_K_M-00001-of-00003.gguf";
     extraProperties = {
       "ctx-size" = "120000";
     };
   };
-  "Mistral-Small-4-119B-2603" = {
-    modelId = "unsloth/Mistral-Small-4-119B-2603-GGUF";
-    filename = "UD-Q4_K_XL/Mistral-Small-4-119B-2603-UD-Q4_K_XL-00001-of-00003.gguf";
-    extraProperties = {
-      "ctx-size" = "65536";
-    };
-  };
+  # "Mistral-Small-4-119B-2603" = {
+  #   modelId = "unsloth/Mistral-Small-4-119B-2603-GGUF";
+  #   filename = "UD-Q4_K_XL/Mistral-Small-4-119B-2603-UD-Q4_K_XL-00001-of-00003.gguf";
+  #   extraProperties = {
+  #     "ctx-size" = "65536";
+  #   };
+  # };
 }
