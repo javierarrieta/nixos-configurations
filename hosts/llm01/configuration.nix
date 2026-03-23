470f8f76388516a6b101df77e68ec3c47793ac4d
Javier Arrieta <javier@techdelivery.es>
2026-03-23 09:01:13 +0000

    [llm01] comfyui group and user ids

--- hosts/llm01/configuration.nix
+++ hosts/llm01/configuration.nix
@@ -166,33 +166,49 @@ in
     };
   };
 
-  users.users.javier = {
-    isNormalUser = true;
-    hashedPasswordFile = config.sops.templates."javier-password".path;
-    extraGroups = [
-      "wheel"
-      "networkmanager"
-      "render"
-      "video"
-    ];
-    shell = pkgs.zsh;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAxtDTZvN/YqOQC1nOGahb/qLp35iYnBTPaGld6/N6k javier@Javiers-MacBook-Air.local"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKhwR+SbHJQR8mSFe5UvBVNlcuG6vpXLU6K+4Rh3z25N javier@DESKTOP-9N12DRJ"
-    ];
+  users.users = {
+    javier = {
+      isNormalUser = true;
+      hashedPasswordFile = config.sops.templates."javier-password".path;
+      extraGroups = [
+        "wheel"
+        "networkmanager"
+        "render"
+        "video"
+      ];
+      shell = pkgs.zsh;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAxtDTZvN/YqOQC1nOGahb/qLp35iYnBTPaGld6/N6k javier@Javiers-MacBook-Air.local"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKhwR+SbHJQR8mSFe5UvBVNlcuG6vpXLU6K+4Rh3z25N javier@DESKTOP-9N12DRJ"
+      ];
+    };
+    ollama = {
+      isSystemUser = true;
+      group = "ollama";
+      extraGroups = [
+        "render"
+        "video"
+      ];
+    };
+    comfyui = {
+      isSystemUser = true;
+      group = "comfyui";
+      uid = 27001;
+      extraGroups = [
+        "render"
+        "video"
+      ];
+    };
   };
 
-  users.users.ollama = {
-    isSystemUser = true;
-    group = "ollama";
-    extraGroups = [
-      "render"
-      "video"
-    ];
-  };
-
-  users.groups.ollama = { };
-
+  users.groups = {
+    ollama = {
+      
+     };
+    comfyui = {
+      gid = 27001;
+    };
+  }
   security.sudo.wheelNeedsPassword = false; # TODO: Remove when issues with passwords are resolved
 
   services = {
