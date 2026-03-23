35f4cd319ef878d206b5c37ded8b048dd3f3b796
Javier Arrieta <javier@techdelivery.es>
2026-03-23 21:24:14 +0000

    [k8s-pi02] fix hardware config

--- hosts/k8s-pi02/hardware-configuration.nix
+++ hosts/k8s-pi02/hardware-configuration.nix
@@ -1,15 +1,28 @@
 {
   config,
-  pkgs,
   lib,
+  pkgs,
   modulesPath,
   ...
 }:
-{
 
+{
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
+
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "usbhid"
+    "usb_storage"
+    "sdhci_pci"
+  ];
+
+  boot.initrd.allowMissingModules = true;
+
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/NIXOS_SD";
@@ -23,4 +36,8 @@
     }
   ];
 
+  networking.useDHCP = false;
+
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+  hardware.enableRedistributableFirmware = lib.mkDefault true;
 }
