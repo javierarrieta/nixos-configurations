456e7b6075df2a2658e5c502dd6f9ab8668cdb31
Javier Arrieta <javier@techdelivery.es>
2026-03-25 11:12:57 +0000

    [k8s-node02] disko configuration

---
+++ hosts/k8s-node02/disko.nix
@@ -0,0 +1,79 @@
+{ modulesPath, ... }:
+{
+  disko.devices = {
+    disk = {
+      disk0 = {
+        device = "/dev/disk/by-id/ata-M.2_SSD_512GB_GSID24C0400225";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "fmask=0077"
+                  "dmask=0077"
+                ];
+              };
+            };
+            root = {
+              size = "50G";
+              content = {
+                type = "luks";
+                name = "disk0-root";
+                extraOpenArgs = [ ];
+                passwordFile = "/tmp/disko-password";
+                settings = {
+                  allowDiscards = true;
+                  crypttabExtraOpts = [ "tpm2-device=auto" ];
+                };
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
+              };
+            };
+            swap = {
+              size = "8G";
+              content = {
+                type = "swap";
+              };
+            };
+            longhorn = {
+              size = "350G";
+              content = {
+                type = "luks";
+                name = "disk0-longhorn";
+                extraOpenArgs = [ ];
+                passwordFile = "/tmp/disko-password";
+                settings = {
+                  allowDiscards = true;
+                  crypttabExtraOpts = [ "tpm2-device=auto" ];
+                };
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/var/lib/longhorn";
+                };
+              };
+            };
+            containerd = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/var/lib/containerd";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
