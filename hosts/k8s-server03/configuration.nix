73c01bc0b948dd4a1d7705e534292d4eb3c6fed8
Javier Arrieta <javier@techdelivery.es>
2026-03-29 10:40:52 +0100

    Revert "[k8s-node03] fix eth name"
    This reverts commit bafc77b56a71a2163fb62d48d328410eba271466.

--- hosts/k8s-server03/configuration.nix
+++ hosts/k8s-server03/configuration.nix
@@ -210,16 +210,13 @@ in
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.interfaces = {
-    enp3s0 = {
-      ipv4.addresses =
-        {
-          address = vars.ipAddress;
-          prefixLength = 24;
-        };
-        useDHCP =  false;
-    };
-  };
+  networking.interfaces.enp2s0.ipv4.addresses = [
+    {
+      address = vars.ipAddress;
+      prefixLength = 24;
+    }
+  ];
+  networking.interfaces.enp2s0.useDHCP = false;
 
   networking.defaultGateway = vars.defaultGateway;
   networking.nameservers = vars.nameservers;
