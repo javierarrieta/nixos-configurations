479fd7df3eccc7adb39dcb6494f4978a652d1f7f
Javier Arrieta <javier@techdelivery.es>
2026-04-06 13:14:21 +0100

    remove nixos-apply alias

--- hosts/k8s-node02/home-manager.nix
+++ hosts/k8s-node02/home-manager.nix
@@ -2,14 +2,6 @@
   config,
   pkgs,
   lib,
-  userOptions,
   ...
 }:
-{
-  programs.fish = {
-    shellAliases = {
-      "nixos-apply" =
-        "cd $HOME/code/nixos-configurations && git pull --ff-only && sudo nixos-rebuild switch --flake .#k8s-node02 ; cd -";
-    };
-  };
-}
+{ }
