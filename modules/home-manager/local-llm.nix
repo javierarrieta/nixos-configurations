ebcd1b22e5ae33dd07847dad3b48117008a42447
Javier Arrieta <--set>
2026-07-07 11:25:26 +0100

    [darwin] autocomplete-server using qwen-2.5-coder-1.5B

--- modules/home-manager/local-llm.nix
+++ modules/home-manager/local-llm.nix
@@ -4,6 +4,6 @@
   home.packages = [ unstablePkgs.llama-cpp ];
 
   programs.fish.shellAliases."autocomplete-server" =
-    "hf download mradermacher/zeta-2.1-i1-GGUF zeta-2.1.i1-IQ3_XS.gguf --local-dir $HOME/llm/models && \
-     llama-server -m $HOME/llm/models/zeta-2.1.i1-IQ3_XS.gguf --temp 0.0 --min-p 0.0 --top-p 1.0 --top-k 1 --repeat-penalty 1.0 --presence-penalty 0.0 --frequency-penalty 0.0 --ctx-size 2048 --batch-size 512 --ubatch-size 512 --cont-batching --flash-attn on --n-gpu-layers 99 --threads 4 --offline --port 8081";
+    "hf download mradermacher/Qwen2.5-Coder-1.5B-i1-GGUF Qwen2.5-Coder-1.5B.i1-Q4_K_M.gguf --local-dir $HOME/llm/models && \
+     llama-server -m $HOME/llm/models/Qwen2.5-Coder-1.5B.i1-Q4_K_M.gguf --temp 0.0 --min-p 0.0 --top-p 1.0 --top-k 1 --repeat-penalty 1.0 --presence-penalty 0.0 --frequency-penalty 0.0 --ctx-size 2048 --batch-size 512 --ubatch-size 512 --cont-batching --flash-attn on --n-gpu-layers 99 --threads 4 --offline --port 8081";
 }
