eaf58257f95660bfbe2122890134d4eecd9d3421
Javier Arrieta <--set>
2026-07-07 11:02:20 +0100

    [darwin] use zeta-2.1 for autocompletion

--- modules/home-manager/local-llm.nix
+++ modules/home-manager/local-llm.nix
@@ -1,9 +1,9 @@
-{ pkgs, ... }:
+{ unstablePkgs, ... }:
 
 {
-  home.packages = [ pkgs.llama-cpp ];
+  home.packages = [ unstablePkgs.llama-cpp ];
 
   programs.fish.shellAliases."autocomplete-server" =
-    "hf download Jackrong/Qwopus3.5-4B-Coder-MTP-GGUF Qwopus3.5-4B-Coder-MTP-Q4_K_M.gguf --local-dir $HOME/llm/models && \
-     llama-server -m $HOME/llm/models/Qwopus3.5-4B-Coder-MTP-Q4_K_M.gguf --temp 0.6 --top-p 0.95 --top-k 20 --min-p 0.1 --presence_penalty 0.0 --repeat-penalty 1.0 --chat-template-kwargs '{\"enable_thinking\": false}' --reasoning-budget -1 --offline -ngl 99 --threads -1 --ctx-size 8192 --port 8081";
+    "hf download mradermacher/zeta-2.1-i1-GGUF zeta-2.1.i1-IQ3_XS.gguf --local-dir $HOME/llm/models && \
+     llama-server -m $HOME/llm/models/zeta-2.1.i1-IQ3_XS.gguf --temp 0.0 --min-p 0.0 --top-p 1.0 --top-k 1 --repeat-penalty 1.0 --presence-penalty 0.0 --frequency-penalty 0.0 --ctx-size 2048 --batch-size 512 --ubatch-size 512 --cont-batching --flash-attn on --n-gpu-layers 99 --threads 4 --offline --port 8081";
 }
