2d9567d23b632559805a25a1dcfc72448ac23dbc
Javier Arrieta <javier@techdelivery.es>
2026-08-02 16:48:34 +0100

    [llm01] llama-server reduce verbosity

--- hosts/llm01/configuration.nix
+++ hosts/llm01/configuration.nix
@@ -263,7 +263,7 @@ in
       LimitMEMLOCK = "infinity";
       WorkingDirectory = "/opt/llm/models";
       CacheDirectory = "llama.cpp";
-      ExecStart = "${llamaPackage}/bin/llama-server --port 8001 --host 0.0.0.0 --models-preset /opt/llm/llama-cpp.ini --flash-attn on --no-mmap --offline -ngl 99 --threads 16";
+      ExecStart = "${llamaPackage}/bin/llama-server --port 8001 --host 0.0.0.0 --models-preset /opt/llm/llama-cpp.ini --flash-attn on --no-mmap --offline -ngl 99 --threads 16 --log-verbosity 2";
       Restart = "on-failure";
       RestartSec = "5s";
     };
