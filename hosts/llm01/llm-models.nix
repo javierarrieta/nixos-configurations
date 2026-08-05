6d547a88ef2d3ad33fd0a2a98f1d313a3aa9ccfa
Javier Arrieta <javier@techdelivery.es>
2026-08-05 16:33:32 +0000

    [llm-01] flash attention per model

--- hosts/llm01/llm-models.nix
+++ hosts/llm01/llm-models.nix
@@ -3,6 +3,7 @@
     modelId = "unsloth/Qwen3.5-2B-GGUF";
     filename = "Qwen3.5-2B-Q4_K_M.gguf";
     extraProperties = {
+      "flash-attn" = "on";
       "ctx-size" = "8192";
       "reasoning-budget" = "-1";
       "temp" = "0.7";
@@ -17,6 +18,7 @@
     modelId = "unsloth/Qwen3.5-4B-GGUF";
     filename = "Qwen3.5-4B-Q4_K_M.gguf";
     extraProperties = {
+      "flash-attn" = "on";
       "ctx-size" = "65536";
     };
   };
@@ -24,6 +26,7 @@
     modelId = "unsloth/Qwen3.5-9B-GGUF";
     filename = "Qwen3.5-9B-Q4_K_M.gguf";
     extraProperties = {
+      "flash-attn" = "on";
       "ctx-size" = "65536";
       "temp" = "0.6";
       "top-k" = "20";
@@ -36,6 +39,7 @@
     modelId = "mradermacher/Mellum-4b-base-GGUF";
     filename = "Mellum-4b-base.Q4_K_M.gguf";
     extraProperties = {
+      "flash-attn" = "on";
       "ctx-size" = "8192";
       "n-predict" = "128";
       "temp" = "0.0";
@@ -51,6 +55,7 @@
   #   modelId = "unsloth/Qwen3.6-35B-A3B-GGUF";
   #   filename = "Qwen3.6-35B-A3B-UD-Q4_K_M.gguf";
   #   extraProperties = {
+  #    "flash-attn" = "on";
   #     "ctx-size" = "200000";
   #   };
   #   mmproj = "mmproj-F32.gguf";
@@ -68,6 +73,7 @@
   #   modelId = "mradermacher/MiroThinker-v1.5-30B-GGUF";
   #   filename = "MiroThinker-v1.5-30B.Q4_K_M.gguf";
   #   extraProperties = {
+  #    "flash-attn" = "on";
   #     "ctx-size" = "65536";
   #   };
   # };
@@ -75,6 +81,7 @@
   #   modelId = "mradermacher/Nemotron-Cascade-2-30B-A3B-i1-GGUF";
   #   filename = "Nemotron-Cascade-2-30B-A3B.i1-Q4_K_M.gguf";
   #   extraProperties = {
+  #     "flash-attn" = "on";
   #     "ctx-size" = "200000";
   #   };
   # };
@@ -82,6 +89,7 @@
   #   modelId = "unsloth/gemma-4-26B-A4B-it-GGUF";
   #   filename = "gemma-4-26B-A4B-it-UD-Q4_K_M.gguf";
   #   extraProperties = {
+  #    "flash-attn" = "on";
   #     "ctx-size" = "200000";
   #   };
   # };
@@ -89,6 +97,7 @@
   #   modelId = "unsloth/gemma-4-12b-it-GGUF";
   #   filename = "gemma-4-12b-it-Q4_K_M.gguf";
   #   extraProperties = {
+  #    "flash-attn" = "on";
   #     "ctx-size" = "200000";
   #   };
   #   mmproj = "mmproj-F16.gguf";
@@ -97,6 +106,7 @@
   #   modelId = "deepreinforce-ai/Ornith-1.0-35B-GGUF";
   #   filename = "ornith-1.0-35b-Q6_K.gguf";
   #   extraProperties = {
+  #    "flash-attn" = "on";
   #     "ctx-size" = "200000";
   #   };
   # };
@@ -104,6 +114,7 @@
     modelId = "mradermacher/VibeThinker-3B-i1-GGUF";
     filename = "VibeThinker-3B.i1-Q6_K.gguf";
     extraProperties = {
+      "flash-attn" = "on";
       "ctx-size" = "200000";
     };
   };
@@ -114,6 +125,7 @@
     # modelDraftModelId = "poolside/Laguna-S-2.1-GGUF";
     extraProperties = {
       "ctx-size" = "200000";
+      "flash-attn" = "off";
       # "spec-type" = "draft-dflash";
       # "spec-draft-n-max" = "15";
     };
