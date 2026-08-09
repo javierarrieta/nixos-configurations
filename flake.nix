1ad86ea65767ceddb068aec9bcfef6fa8e4aa228
Javier Arrieta <javier@techdelivery.es>
2026-08-09 20:55:23 +0100

    Update to nixpkgs release-26.05 and home-manager release-26.05

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+      home-manager = {
+        url = "github:nix-community/home-manager/release-26.05";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     comin = {
