d769ff7963ada6efc1b91bb9faf4c1ac72764c31
Javier Arrieta <javier@techdelivery.es>
2026-08-23 08:50:58 +0100

    fix: back up pre-existing dotfiles instead of failing the switch
    Workspaces can hold pre-HM config files; clobbering them aborted the
    whole switch. Move conflicts aside with a .pre-hm suffix.

--- home/hosts/coder-workspace/home.nix
+++ home/hosts/coder-workspace/home.nix
@@ -5,6 +5,9 @@
 
   home.stateVersion = "25.11";
 
+  # Workspaces may hold pre-HM dotfiles; back them up instead of failing.
+  home-manager.backupFileExtension = "pre-hm";
+
   home.sessionVariables = {
     NIX_CONFIG = "experimental-features = nix-command flakes";
   };
