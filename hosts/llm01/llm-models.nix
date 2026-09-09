b6696a421b013ca9b90918542514d65b7798edc1
javierarrieta <javier.arrieta@gmail.com>
2026-09-09 20:24:13 +0000

    [llm01] reasoning off for instruct models

--- hosts/llm01/llm-models.nix
+++ hosts/llm01/llm-models.nix
@@ -13,7 +13,7 @@
       "top-p" = "0.80";
       "min-p" = "0.0";
       "repeat-penalty" = "1.0";
-      "chat-template-kwargs" = "{\"enable_thinking\": false}";
+      "reasoning" = "off";
     };
   };
   "Qwen3.5-4B" = {
@@ -54,7 +54,7 @@
       "top-p" = "0.80";
       "min-p" = "0.0";
       "repeat-penalty" = "1.0";
-      "chat-template-kwargs" = "{\"enable_thinking\": false}";
+      "reasoning" = "off";
     };
   };
   "Mellum-4B" = {
